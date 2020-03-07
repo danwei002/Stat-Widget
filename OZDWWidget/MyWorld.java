8cf830a2daa11c4c7617e39664d9dfd57e7687f1
danwei002 <35182369+danwei002@users.noreply.github.com>
2020-03-06 23:57:50 -0500

    Played around with healthbars/charge bars

--- OZDWWidget/MyWorld.java
+++ OZDWWidget/MyWorld.java
@@ -16,8 +16,8 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
-        StatBar stat = new StatBar();
+        super(1200, 800, 1); 
+        StatBar stat = new StatBar(false, 800, 200, 10);
         addObject(stat, getWidth()/2, getHeight()/2);
     }
 }
--- OZDWWidget/StatBar.java
+++ OZDWWidget/StatBar.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.lang.Math;
 /**
  * Write a description of class StatBar here.
  * 
@@ -10,51 +10,198 @@ public class StatBar extends Actor
 {
     // Instance variables
     
+    // Bar type
+    private boolean isHealthBar;
+    
+    // Track if the ult fully charged sound has been played
+    private boolean playedUltSound;
+    
+    // Track if the low HP warning noie is currently playing
+    private boolean playingLowHPSound;
+    
     // Track values
-    private int currHp;
-    private int maxHp;
-    private int currShield;
-    private int maxShield;
-    private int currCharge;
-    private int maxCharge;
+    private int currVal;
+    private int maxVal;
     
     // Bar colors
-    private Color hpFilled;
-    private Color hpEmpty;
-    private Color shieldFilled;
-    private Color shieldEmpty;
-    private Color chargeFilled;
-    private Color chargeEmpty;
+    private Color filledColor;
+    private Color emptyColor;
+    private Color borderColor;
     
     // Bar dimensions
-    private int hpWidth;
-    private int hpHeight;
-    private int shieldWidth;
-    private int shieldHeight;
-    private int chargeRadius;
-
-    public StatBar()
+    private int width;
+    private int height;
+    private int borderWidth;
+    
+    // Sounds
+    private GreenfootSound ultCharged = new GreenfootSound("UltimateCharged.mp3");
+    
+    // Source: https://www.youtube.com/watch?v=j0sJ4RoFTug
+    private GreenfootSound lowHPSound = new GreenfootSound("LowHealthNoise.mp3");
+    
+    // Canvas
+    private GreenfootImage img;
+    
+    public StatBar(boolean barType)
     {
-        GreenfootImage myImage = new GreenfootImage(200, 50);
-        maxHp = 100;
-        maxShield = 100;
-        maxCharge = 100;
+        // Default width and height
+        width = 300;
+        height = 50;
+        borderWidth = 5;
         
-        myImage.setColor(Color.GREEN);
-        myImage.fillRect(0, 0, 150, 25);
-        setImage(myImage);
+        // Set the maximum value for the bar
+        maxVal = 100;
         
-        myImage.setColor(Color.RED);
-        myImage.fillRect(100, 0, 50, 25);
-        setImage(myImage);
+        playedUltSound = false;
+        playingLowHPSound = false;
+        
+        // Image to draw on
+        img = new GreenfootImage(width, height);
+        
+        // Either a health/shield/hitpoint bar or a "charge" bar (tracks ability charge/progression)
+        isHealthBar = barType;
+        
+        // Creating the border
+        borderColor = Color.DARK_GRAY;
+        img.setColor(borderColor);
+        img.fillRect(0, 0, width, height);
+        
+        // Depending on the bar type, create the bar using a set color
+        if (isHealthBar)
+        {
+            filledColor = Color.GREEN;
+            emptyColor = Color.RED;
+            img.setColor(filledColor);
+            img.fillRect(borderWidth, borderWidth, width - borderWidth * 2, height - borderWidth  * 2);
+        }
+        else
+        {
+            filledColor = Color.ORANGE;
+            emptyColor = Color.BLACK;
+            img.setColor(emptyColor);
+            img.fillRect(borderWidth, borderWidth, width - borderWidth * 2, height - borderWidth  * 2);
+        }
+        
+        setImage(img);
     }
     
+    public StatBar(boolean barType, int width, int height, int borderWidth)
+    {
+        // Setting dimensions & bar type
+        this.width = width;
+        this.height = height;
+        isHealthBar = barType;
+        this.borderWidth = borderWidth;
+        
+        playedUltSound = false;
+        playingLowHPSound = false;
+        
+        // Set the maximum value for the bar
+        maxVal = 100;
+        
+        // Image to draw on
+        img = new GreenfootImage(width, height);
+        
+        // Creating the border
+        borderColor = Color.DARK_GRAY;
+        img.setColor(borderColor);
+        img.fillRect(0, 0, width, height);
+        
+        // Depending on the bar type, create the bar using a set color
+        if (isHealthBar)
+        {
+            filledColor = Color.GREEN;
+            emptyColor = Color.RED;
+            img.setColor(filledColor);
+            img.fillRect(borderWidth, borderWidth, width - borderWidth * 2, height - borderWidth  * 2);
+        }
+        else
+        {
+            filledColor = Color.ORANGE;
+            emptyColor = Color.BLACK;
+            img.setColor(emptyColor);
+            img.fillRect(borderWidth, borderWidth, width - borderWidth * 2, height - borderWidth  * 2);
+        }
+        
+        setImage(img);
+    }
+
     /**
      * Act - do whatever the StatBar wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int test = 30;
     public void act() 
     {
+        // Just some testing code
+        if (Greenfoot.getKey() == "space")
+        {
+            ultCharged.play();
+        }
+        update(test);
+        if (Greenfoot.isKeyDown("A"))
+        {
+            test--;
+        }
+        else if (Greenfoot.isKeyDown("D"))
+        {
+            test++;
+        }
+    }
+    
+    public void update(int newValue)
+    {
+        // Can't have a negative amount of charge/health
+        if (newValue < 0) {return;}
+        
+        
+        if (isHealthBar) // Sound playing if statement for healthbars
+        {
+            if (!playingLowHPSound && newValue * 10 <= maxVal * 3) // If the updated value is less than 30% of the maximum, and the sound was not previously playing, play the sound
+            {
+                lowHPSound.playLoop();
+                playingLowHPSound = true;
+            }
+            else
+            {
+                if (newValue * 10 > maxVal * 3) // Once the updated value exceeds 30% of the maximum, no longer critical health, stop the sound
+                {
+                    playingLowHPSound = false;
+                    lowHPSound.stop();
+                }
+            }
+        }
+        else // Sound playing if statement for charge bars
+        {
+            if (newValue == maxVal && !playedUltSound) 
+            {
+                ultCharged.play();
+                playedUltSound = true;
+            }
+            else
+            {
+                // Reset the sound tracking boolean
+                if (newValue < maxVal)
+                {
+                    playedUltSound = false;
+                }
+            }
+            
+            // Can't exceed the max value
+            if (newValue > maxVal) 
+            {
+                newValue = maxVal;
+            }
+        }
+        
+        
+        int filledWidth = (width - borderWidth * 2 ) * newValue / maxVal;
+        int emptyWidth = (width - borderWidth * 2) - filledWidth;
 
-    }    
+        img.setColor(filledColor);
+        img.fillRect(borderWidth, borderWidth, filledWidth, height - borderWidth * 2);
+        img.setColor(emptyColor);
+        img.fillRect(borderWidth + filledWidth, borderWidth, emptyWidth, height - borderWidth * 2);
+        setImage(img);
+    }
 }
